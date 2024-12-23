be746fe14830de6e1cdf80ec9279da52a4f87b60
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-12-23 21:29:22 +0530

    xp

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,26 +1,21 @@
 "use client"
 
-import * as React from "react"
+import { useState } from "react"
 import Link from "next/link"
 import { Menu, Search } from "lucide-react"
 
 export default function Navbar() {
-  const [state, setState] = React.useState(false)
-
-  const menus = [
-    { title: "Home", path: "/your-path" },
-    { title: "Blog", path: "/your-path" },
-    { title: "About Us", path: "/your-path" },
-    { title: "Contact Us", path: "/your-path" },
-  ]
+  const [state, setState] = useState(false);
 
   return (
     <nav className="bg-white w-full border-b md:border-0">
       <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+        
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
           <Link href="/">
-            <h1 className="text-3xl font-bold text-purple-600">Logo</h1>
+            <h1 className="text-3xl font-bold text-blue-600">Owl</h1>
           </Link>
+
           <div className="md:hidden">
             <button
               className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
@@ -30,25 +25,25 @@ export default function Navbar() {
             </button>
           </div>
         </div>
+
         <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+          className={`${
             state ? "block" : "hidden"
-          }`}
+          } md:block md:justify-end flex-1 pb-3 mt-8 md:pb-0 md:mt-0`}
         >
-          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            {menus.map((item, idx) => (
-              <li key={idx} className="text-gray-600 hover:text-indigo-600">
-                <Link href={item.path}>{item.title}</Link>
-              </li>
-            ))}
-            <form className="flex items-center  space-x-2 border rounded-md p-2">
-              <Search className="h-5 w-5 flex-none text-gray-300" />
-              <input
-                className="w-full outline-none appearance-none placeholder-gray-500 text-gray-500 sm:w-auto"
-                type="text"
-                placeholder="Search"
-              />
-            </form>
+          <ul className="flex flex-col md:flex-row justify-end space-y-6 md:space-x-6 md:space-y-0">
+            <li className="text-gray-600 hover:text-blue-600">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="text-gray-600 hover:text-blue-600">
+              <Link href="/">Xplore</Link>
+            </li>
+            {/* <li className="text-gray-600 hover:text-blue-600">
+              <Link href="/">About Us</Link>
+            </li>
+            <li className="text-gray-600 hover:text-blue-600">
+              <Link href="/">Contact Us</Link>
+            </li> */}
           </ul>
         </div>
       </div>
