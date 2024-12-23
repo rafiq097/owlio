0f0826ff32da498312f681a9cea1944e34cbaa9e
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-12-23 22:06:17 +0530

    form

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/app/components/Navbar.js";
+import InputForm from "@/app/components/Form.js";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <InputForm />
     </div>
   );
 }
