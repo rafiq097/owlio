441e6e7723834c56b6f659f469af9dd4a0a7871d
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-12-25 08:54:22 +0530

    nm

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,136 +4,131 @@ import connectToDB from "@/lib/db.js";
 import User from "@/models/User.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
+const JWT_SECRET = process.env.JWT_SECRET || "DEFAULT_KEY";
+
+// Helper function to set cookies
+const setCookie = (res, name, value, options = {}) => {
+  const cookieOptions = {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    ...options,
+  };
+
+  const cookieString = Object.entries(cookieOptions)
+    .map(([key, val]) => (val === true ? key : `${key}=${val}`))
+    .join("; ");
+
+  res.setHeader("Set-Cookie", `${name}=${value}; ${cookieString}`);
+};
+
+// Register User
 export async function registerUserAction(formData) {
-    await connectToDB();
-    try {
-        const { username, password } = formData;
+  await connectToDB();
+  try {
+    const { username, password } = formData;
 
-        const checkUser = await User.findOne({ username });
-        console.log(checkUser);
-        if (checkUser) {
-            return {
-                success: false,
-                message: "User already exists ! Please try with different username",
-            };
-        }
-
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(password, salt);
-
-        const newlyCreatedUser = new User({
-            username,
-            password: hashedPassword,
-        });
-
-        const savedUser = await newlyCreatedUser.save();
-
-        if (savedUser) {
-            return {
-                success: true,
-                data: JSON.parse(JSON.stringify(savedUser)),
-            };
-        } else {
-            return {
-                success: false,
-                message: "Something went wrong! Please try again",
-            };
-        }
-    } catch (error) {
-        console.log(error);
-        return {
-            message: "Something error occured",
-            success: false,
-        };
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return {
+        success: false,
+        message: "User already exists! Please try with a different username.",
+      };
     }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(savedUser)),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Something went wrong! Please try again.",
+    };
+  }
 }
 
-export async function loginUserAction(formData) {
-    await connectToDB();
-    try {
-        const { username, password } = formData;
+// Login User
+export async function loginUserAction(formData, res) {
+  await connectToDB();
+  try {
+    const { username, password } = formData;
 
-        //check if user exists in DB
-        const checkUser = await User.findOne({ username });
-        if (!checkUser) {
-            return {
-                success: false,
-                message: "User doesnot exist ! please sign up",
-            };
-        }
-
-        //check if password is valid or not
-        const checkPassword = await bcryptjs.compare(password, checkUser.password);
-        if (!checkPassword) {
-            return {
-                message: "Password is incorrect please check",
-                success: false,
-            };
-        }
-
-        const createdTokenData = {
-            id: checkUser._id,
-            username: checkUser.username
-        };
-
-        const token = jwt.sign(createdTokenData, "DEFAULT_KEY", {
-            expiresIn: "30d",
-        });
-
-        const getCookies = cookies();
-        getCookies.set("token", token);
-
-        return {
-            success: true,
-            message: "Login is successfull",
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            success: false,
-            message: "Something went wrong! please try again",
-        };
+    const user = await User.findOne({ username });
+    if (!user) {
+      return {
+        success: false,
+        message: "User does not exist! Please sign up.",
+      };
     }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return {
+        success: false,
+        message: "Incorrect password. Please try again.",
+      };
+    }
+
+    const tokenData = { id: user._id, username: user.username };
+    const token = jwt.sign(tokenData, JWT_SECRET, { expiresIn: "30d" });
+
+    setCookie(res, "token", token, { maxAge: 30 * 24 * 60 * 60 });
+
+    return {
+      success: true,
+      message: "Login successful.",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Something went wrong! Please try again.",
+    };
+  }
 }
 
-export async function fetchAuthUserAction() {
-    await connectToDB();
-    try {
-        const getCookies = cookies();
-        const token = getCookies.get("token")?.value || "";
-        if (token === "") {
-            return {
-                success: false,
-                message: "Token is invalid",
-            };
-        }
-
-        const decodedToken = jwt.verify(token, "DEFAULT_KEY");
-        const getUserInfo = await User.findOne({ _id: decodedToken.id });
-
-        if (getUserInfo) {
-            return {
-                success: true,
-                data: JSON.parse(JSON.stringify(getUserInfo)),
-            };
-        } else {
-            return {
-                success: false,
-                message: "Some error occured ! Please try again",
-            };
-        }
-    } catch (error) {
-        console.log(error);
-        return {
-            success: false,
-            message: "Something went wrong! please try again",
-        };
+// Fetch Authenticated User
+export async function fetchAuthUserAction(req) {
+  await connectToDB();
+  try {
+    const token = req.cookies?.token || "";
+    if (!token) {
+      return { success: false, message: "Token is invalid" };
     }
+
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    const user = await User.findOne({ _id: decodedToken.id });
+
+    if (user) {
+      return {
+        success: true,
+        data: JSON.parse(JSON.stringify(user)),
+      };
+    } else {
+      return { success: false, message: "User not found" };
+    }
+  } catch (error) {
+    console.error(error);
+    return { success: false, message: "Invalid token or server error" };
+  }
 }
 
-export async function logoutAction() {
-    const getCookies = cookies();
-    getCookies.set("token", "");
+// Logout
+export async function logoutAction(res) {
+  setCookie(res, "token", "", { maxAge: -1 });
+  return { success: true, message: "Logged out successfully." };
 }
--- src/lib/db.js
+++ src/lib/db.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
         console.log("Connected to MongoDB.");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1);
+        // process.exit(1);
     }
 };
 
