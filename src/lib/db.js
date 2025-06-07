6a74ecbe4c74a0a0a8ed564b874a4c2b4f9a4371
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-06-07 15:07:53 +0530

    changes in db

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,10 +7,7 @@ const connectDB = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB.");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
